04705b061200bd08a2f8556c45202411f9b7595a
crisis82 <filmerlo99@gmail.com>
2022-10-13 11:06:32 +0200

    renamed module remote-control

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 # MagicMirror
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo pacman -Syu nodejs
+cp utils/config/config.json MagicMirror/config/
+cp -r utils/modules/ MagicMirror/modules/
+bash -c MagicMirror/modules/MMM-Remote-Control/installer.sh
 cd MagicMirror/
 npm run install-mm
 cd ..
